21e8451dee0893528eb9bdccc7495b58e8de7693
Ville Tirronen <ville.e.t.tirronen@jyu.fi>
2011-01-27 15:12:30 +0200

    Updated examples/edges.hs

--- examples/edges.hs
+++ examples/edges.hs
@@ -6,9 +6,9 @@ main = do
     Just x <- loadImage "smallLena.jpg"
     saveImage "edges.png" $ montage (3,2) 5 $
         [x
-        ,sobel (1,0) 5 x
-        ,sobel (0,1) 5 x
-        ,laplace 5 x
+        ,sobel (1,0) s5 x
+        ,sobel (0,1) s5 x
+        ,laplace l5 x
         ,unsafeImageTo32F $ susan (5,5) 0.5 x
         ,unsafeImageTo32F $ canny 20 40 5 (unsafeImageTo8Bit x)
         ]
