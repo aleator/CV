1d2ed3d1b37a88b7d0f6e57b556119ecd4db6961
Ville Tirronen <ville.e.t.tirronen@jyu.fi>
2011-03-18 16:01:42 +0200

    Improved example for distance transform

--- examples/distance.hs
+++ examples/distance.hs
@@ -4,12 +4,18 @@ import CV.Image
 import CV.Transforms
 import CV.ColourUtils
 import qualified CV.ImageMath as IM
+import CV.Drawing
+import CV.ImageOp
+
+import Data.Maybe (fromJust)
 
 main = do
-    Just x <- loadImage "smallLena.jpg"
-    saveImage "distance.png"  $ montage (2,1) 2 
-                                 [x
-                                 ,stretchHistogram $ 
-                                    distanceTransform CV_DIST_L2 M3 (IM.moreThan 0.6 x)
+    x <- loadImage "smallLena.jpg" >>= return . IM.moreThan 0.6 . fromJust
+    let dtf = distanceTransform CV_DIST_L2 M3 x
+        (_,((mx,my),v)) = IM.findMinMaxLoc dtf
+    saveImage "distance.png"  $ montage (3,1) 2 
+                                 [unsafeImageTo32F x
+                                 ,(stretchHistogram $ dtf)
+                                 ,(stretchHistogram $ dtf) <# circleOp 1 (mx,my) (round v) Filled
                                  ]
 
