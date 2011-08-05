12848849013a57b872ddc87f8eeb3ad37828326b
Ville Tirronen <aleator@gmail.com>
2011-08-05 10:53:13 +0300

    Example for finding chessboard corners

--- examples/Chessboard.hs
+++ examples/Chessboard.hs
@@ -4,7 +4,7 @@ import CV.Calibration
 
 main = do
     Just i <- loadColorImage "chess.png"
-    let corners = findChessboardCorners (unsafeImageTo8Bit i) (4,5) []
+    let corners = findChessboardCorners (unsafeImageTo8Bit i) (4,5) defaultFlags
     let y = drawChessboardCorners (unsafeImageTo8Bit i) (4,5) corners
     mapM_ print (corners)
     saveImage "found_chessboard.png" y
