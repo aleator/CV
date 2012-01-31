8c0d4ff53b25cce50c5bf0d9b5e7988f525e4f88
Ville Tirronen <aleator@gmail.com>
2012-01-31 15:15:05 +0200

    Pixelwise modifications

--- examples/pixelwise.hs
+++ examples/pixelwise.hs
@@ -10,24 +10,19 @@ import Control.Applicative hiding ((<**>))
 
 main = do
     Just x <- loadImage "smallLena.jpg"
-    let  y = T.flip T.Horizontal x
-         u = T.flip T.Vertical   y
-         z = T.flip T.Vertical   x
+    let  y = fromImage $ T.flip T.Horizontal x
+         u = fromImage $ T.flip T.Vertical   y
+         z = fromImage $ T.flip T.Vertical   x
     saveImage "PixelWise.png" $ montage (3,3) 5 
-        [x
-        ,y
-        ,{-#SCC "+" #-} stretchHistogram . toImage $ ((+) <$$> x <+> y)
-        
-        ,{-#SCC "++++" #-} stretchHistogram . toImage $ ((\a b c d -> a+b+c+d) <$$> x 
-                                                           <+> y
-                                                           <+> u
-                                                           <+> z)
-        ,{-#SCC "sin" #-}stretchHistogram . toImage $ fmap (sin . (*9)) . fromImage $ x 
-        ,stretchHistogram . toImage $ fmap log . fromImage $ x 
+        [x,y
+        ,stretchHistogram . toImage $ (z + y)
+        ,stretchHistogram . toImage $ y + u + z
+        ,stretchHistogram . toImage $ fmap (sin . (*9)) $ y 
+        ,stretchHistogram . toImage $ fmap log $ x 
         
         ,stretchHistogram . gaussian (3,3) 
                           . toImage $ atan2 <$$> (sobel (1,0) s5 x) 
                                             <+>  (sobel (0,1) s5 x) 
-        ,toImage $ fmap (\x -> if x > 0.5 then 0 else 1) . fromImage $ x
-        ,toImage $ fmap (\x -> if x > 0.5 && x < 0.6 then 0 else 1) . fromImage $ x
+        ,toImage $ fmap (\x -> if x > 0.5 then 0 else 1) $ x
+        ,toImage $ fmap (\x -> if x > 0.5 && x < 0.6 then 0 else 1)  $ x
         ]
