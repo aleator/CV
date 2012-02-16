a2e341f8e779a210612e8ae12b8fc24e0368da69
Ville Tirronen <ville.tirronen@jyu.fi>
2012-02-16 14:33:24 +0200

    Better rectangles

--- Utils/GeometryClass.hs
+++ Utils/GeometryClass.hs
@@ -9,6 +9,10 @@ class Point2D a where
 convertPt :: (Point2D a, Point2D b, ELP a ~ ELP b) => a -> b
 convertPt = toPt . pt
 
+class BoundingBox a where
+   type ELBB a :: *
+   bounds :: a -> (ELBB a, ELBB a, ELBB a, ELBB a)
+
 class Line2D a where
    type ELL a :: *
    offsetAngle :: a -> (ELL a, Double)
--- Utils/Rectangle.hs
+++ Utils/Rectangle.hs
@@ -4,23 +4,27 @@ import Test.LazySmallCheck
 import Utils.Point
 import Control.DeepSeq
 
-newtype Rectangle a = Rectangle ((a,a),(a,a)) deriving (Eq,Show)
+data Rectangle a = Rectangle {-#UNPACK#-} !a
+                             {-#UNPACK#-} !a
+                             {-#UNPACK#-} !a
+                             {-#UNPACK#-} !a
+                             deriving (Eq,Show)
 
 a `s` b = rnf a `seq` b
 
 instance (NFData a) => NFData (Rectangle a) where
-    rnf (Rectangle ((a,b),(c,d))) = (a `s` b `s` c `s` d) `seq` ()
+    rnf (Rectangle a b c d) = (a `s` b `s` c `s` d) `seq` ()
 
-left   (Rectangle ((x,y),(w,h))) = x
-right  (Rectangle ((x,y),(w,h))) = x+w
-top    (Rectangle ((x,y),(w,h))) = y
-bottom (Rectangle ((x,y),(w,h))) = y+h
-topLeft  (Rectangle ((x,y),(w,h))) = (x,y)
-topRight (Rectangle ((x,y),(w,h))) = (x+w,y)
-bottomLeft (Rectangle ((x,y),(w,h))) = (x,y+h)
-bottomRight (Rectangle ((x,y),(w,h))) = (x+w,y+h)
+left   (Rectangle x y w h) = x
+right  (Rectangle x y w h) = x+w
+top    (Rectangle x y w h) = y
+bottom (Rectangle x y w h) = y+h
+topLeft  (Rectangle x y w h) = (x,y)
+topRight (Rectangle x y w h) = (x+w,y)
+bottomLeft (Rectangle x y w h) = (x,y+h)
+bottomRight (Rectangle x y w h) = (x+w,y+h)
 vertices r = [topLeft r, topRight r, bottomLeft r, bottomRight r]
-rSize (Rectangle ((x,y),(w,h))) = (w,h)
+rSize (Rectangle x y w h) = (w,h)
 rArea r = let (w,h) = rSize r in (w*h)
 
 -- TODO: Add documentation #Cleanup
@@ -29,17 +33,17 @@ instance (Num a, Ord a , Serial a) => Serial (Rectangle a) where
     series = cons4 $ \a b c d -> mkRectangle (a,b) (c,d)
 
 -- | Create rectangle around point (x,y)
-around (x,y) (w,h) = mkRectangle (x', y') (w,h)
+around (x,y) (w,h) = mkRectangle (x',y') (w,h)
     where (x',y') = (x-(w/2),y-(h/2))
 
-mkRectangle (x,y) (w,h) = Rectangle ((x-negW,y-negH),(abs w,abs h))
+mkRectangle (x,y) (w,h) = Rectangle (x-negW) (y-negH) (abs w) (abs h)
     where
      negH | h<0  = abs h
           | h>=0 = 0
      negW | w<0  = abs w
           | w>=0 = 0
 
-mkRectCorners (x1,y1) (x2,y2) = Rectangle ((x,y),(w,h))
+mkRectCorners (x1,y1) (x2,y2) = Rectangle x y w h
  where
     x = min x1 x2
     y = min y1 y2
@@ -51,14 +55,16 @@ prop_Corners p w = mkRectCorners p (p+w) == mkRectangle p w
 
 mkRec = uncurry mkRectangle
 
+fromPtSize (x,y) (w,h) = Rectangle x y w h
+
 -- | Return rectangle r2 in coordinate system defined by r1
-inCoords r1 r2@(Rectangle (pos,size)) = Rectangle (pos-topLeft r1,size )
+inCoords r1 r2@(Rectangle x y w h) = fromPtSize ((x,y)-topLeft r1,(w,h) )
 
 -- | Return a point in coordinates of given rectangle
 inCoords' r1 pt = pt - topLeft r1
 
 -- | Adjust the size of the rectangle to be divisible by 2^n.
-enlargeToNthPower n (Rectangle ((x,y),(w,h))) = Rectangle ((x,y),(w2,h2))
+enlargeToNthPower n (Rectangle x y w h) = Rectangle x y w2 h2
     where
      (w2,h2) = (pad w, pad h)
      pad x = x + (np - x `mod` np)
@@ -108,13 +114,12 @@ tile tilesize overlap r = [mkRectangle ((x,y)-overlap) tilesize
      endy = bottom r+snd overlap
 
 -- | Scale a rectangle
-scale (a,b) (Rectangle ((x,y),(s1,s2)))
+scale (a,b) (Rectangle x y s1 s2)
     = mkRectangle (round (a*fromIntegral x),round (b*fromIntegral y))
                   (round (a*fromIntegral s1),round (b*fromIntegral s2))
 
 
-toInt (Rectangle (p, s))
-    = Rectangle (both round p
-                ,both round s)
- where both f (a,b) = (f a , f b)
+toInt (Rectangle a b c d)
+    = Rectangle (f a) (f b) (f c) (f d)
+ where f = fromIntegral
 
