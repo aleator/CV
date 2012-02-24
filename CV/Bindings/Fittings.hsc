a9ad39021d5f96040028962caa95ab8e829d9bd1
Ville Tirronen <aleator@gmail.com>
2012-02-24 13:11:06 +0200

    Added wrapper for cvConvexHull2

--- CV/Bindings/Fittings.hsc
+++ CV/Bindings/Fittings.hsc
@@ -11,8 +11,10 @@ import CV.Bindings.Types
 
 #ccall wrapFitEllipse , Ptr <CvArr> -> Ptr <CvBox2D> -> IO ()
 #ccall cvFitLine      , Ptr <CvArr> -> CInt -> Double  -> Double -> Double-> Ptr Float -> IO ()
+#ccall cvConvexHull2  , Ptr <CvArr> -> Ptr () -> CInt -> CInt -> IO ()
 #ccall wrapMinAreaRect2 , Ptr <CvArr> -> Ptr <CvMemStorage> -> Ptr <CvBox2D> -> IO ()
 #ccall wrapBoundingRect , Ptr <CvArr> -> CInt -> Ptr <CvRect> -> IO ()
+#ccall cvMinEnclosingCircle, Ptr <CvArr> -> Ptr <CvPoint2D32f> -> Ptr CFloat -> IO ()
 
 #num CV_DIST_USER    
 #num CV_DIST_L1      
--- examples/Fitting.hs
+++ examples/Fitting.hs
@@ -8,14 +8,20 @@ import CV.ImageOp
 main = do
         let res :: Image GrayScale D32
             res = empty (400,400)
-            testPts = [(200+100*sin x-80*cos x,200+60*cos x) | x <- [0,0.1..2*pi]]
+            testPts = [(200+100*sin x-80*cos x,200+60*cos x) | x <- [0,0.1..pi]] ++
+                      [(150+100*sin (-x)+20*cos x,100+60*cos (-x)) | x <- [0,0.1..2*pi]]
             mat = fromList (1,length testPts) testPts
             ell = fitEllipse mat
             bb  = minAreaRect mat
             br  = boundingRect mat
+            ch  = map (both round) . toList $ convexHull mat 
+            segments = zip (ch) (tail . cycle $ ch)
             pts = res <## [circleOp 1 (round x, round y) 3 Filled | (x,y) <- testPts]
+                      <#  drawLinesOp 0.5 2 segments
                       <# drawBox2Dop 1 bb
         saveImage "bb_result.png" pts
         print ell
         print bb
         print br
+
+both f (a,b)  = (f a, f b)
